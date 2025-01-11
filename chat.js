87cef9906549cf92ce6051bcf70f2ec15f1e1ba1
Thúlio <42282639+thulioprado@users.noreply.github.com>
2025-01-11 07:00:13 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,25 +1,28 @@
-const outputDiv = getById('output');
+(function() {
+    const outputDiv = document.getElementById('output');
 
-const callback = (mutationsList, observer) => {
-    mutationsList.forEach(mutation => {
-        if (mutation.type === 'childList') {
-            mutation.addedNodes.forEach(node => {
-                if (node.classList && node.classList.contains('highlight-chat')) {
-                    const nameElement = node.querySelector('.hl-name');                    
-                    if (nameElement) {
-                        const currentColor = nameElement.style.color;
-                        if (currentColor) {
-                            nameElement.style.background = currentColor;
-                            nameElement.style.color = 'white'; 
+    if (outputDiv) {
+        const callback = (mutationsList, observer) => {
+            mutationsList.forEach(mutation => {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(node => {
+                        if (node.classList && node.classList.contains('highlight-chat')) {
+                            const nameElement = node.querySelector('.hl-name');                    
+                            if (nameElement) {
+                                const currentColor = nameElement.style.color;
+                                if (currentColor) {
+                                    nameElement.style.background = currentColor;
+                                    nameElement.style.color = 'white'; 
+                                }
+                            }
                         }
-                    }
+                    });
                 }
             });
-        }
-    });
-};
-
-
-const observer = new MutationObserver(callback);
-const config = { childList: true, subtree: true };
-observer.observe(outputDiv, config);
+        };
+        
+        const observer = new MutationObserver(callback);
+        const config = { childList: true, subtree: true };
+        observer.observe(outputDiv, config);
+    }
+})();
