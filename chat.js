9f9461cc042a8ccfb000fa3575041005834333c8
Thúlio <42282639+thulioprado@users.noreply.github.com>
2025-01-11 06:45:12 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,25 @@
+const outputDiv = document.getElementById('output');
+
+const callback = (mutationsList, observer) => {
+    mutationsList.forEach(mutation => {
+        if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach(node => {
+                if (node.classList && node.classList.contains('highlight-chat')) {
+                    const nameElement = node.querySelector('.hl-name');                    
+                    if (nameElement) {
+                        const currentColor = nameElement.style.color;
+                        if (currentColor) {
+                            nameElement.style.background = currentColor;
+                            nameElement.style.color = 'white'; 
+                        }
+                    }
+                }
+            });
+        }
+    });
+};
+
+
+const observer = new MutationObserver(callback);
+const config = { childList: true, subtree: true };
+observer.observe(outputDiv, config);
